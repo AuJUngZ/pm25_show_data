27ffc1a6eec4445801d70c5d344a62867ad2c293
AuJung <natthaphong_th@cmu.ac.th>
2023-04-09 18:34:43 +0700

    - update to make website update date automatically. - decorate Table.tsx to make them more beautiful.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {IconPhoto} from "@tabler/icons-react";
 import axios from "axios";
-import {DatePicker, LocalizationProvider, TimeClock, TimeField} from '@mui/x-date-pickers';
+import {DatePicker, LocalizationProvider, TimeField} from '@mui/x-date-pickers';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from "dayjs";
 
@@ -12,16 +12,14 @@ interface Props {
 export default function Table({listOfCity}: Props) {
     const [sortedBy, setSortedBy] = useState<string>("value ASC");
     const [result, setResult] = useState<any>([]);
-    const [startDate, setStartDate] = useState<string>("2023-04-09");
-    const [endDate, setEndDate] = useState<string>("2023-04-09");
+    const [startDate, setStartDate] = useState<string>(getCurrentDate);
+    const [endDate, setEndDate] = useState<string>(getCurrentDate());
     const [startTime, setStartTime] = useState<string>("08:00:00");
-    const [endTime, setEndTime] = useState<string>("09:00:00");
-
+    const [endTime, setEndTime] = useState<string>("23:00:00");
 
     //GET DATA FROM API
     const fetchData = async (temp: string[]) => {
         axios.defaults.baseURL = 'http://localhost/pm25';
-        console.log("start_date: " + startDate + " end_date: " + endDate + " start_time: " + startTime + " end_time: " + endTime);
         const response = await axios.get(`/data/fetch/${temp[0]}`, {
             headers: {
                 "sort_order": temp[1],
@@ -31,7 +29,6 @@ export default function Table({listOfCity}: Props) {
                 "end_time": endTime
             }
         });
-        console.log(response.data)
         setResult(response.data);
     }
 
@@ -42,19 +39,24 @@ export default function Table({listOfCity}: Props) {
         } catch (e) {
             console.log(e)
         }
-    }, [sortedBy, startDate, endDate, startTime, endTime,listOfCity])
+    }, [sortedBy, startDate, endDate, startTime, endTime, listOfCity])
 
 
+    function getCurrentDate() {
+        // @ts-ignore
+        const {$D: dd, $M, $y: yyyy} = dayjs(new Date());
+        const mm = $M + 1;
+        return `${yyyy}-${mm}-${dd}`
+    }
 
-    const handleStartDate = (e: any) =>{
-        const yyyy = e.$y;
-        const mm = e.$M + 1;
-        const dd = e.$D;
+    const handleStartDate = (e: any) => {
+        const {$y: yyyy, $M, $D: dd} = e;
+        const mm = $M + 1;
         const date = `${yyyy}-${mm}-${dd}`;
         setStartDate(date)
     }
 
-    const handleEndDate = (e: any) =>{
+    const handleEndDate = (e: any) => {
         const yyyy = e.$y;
         const mm = e.$M + 1;
         const dd = e.$D;
@@ -62,7 +64,7 @@ export default function Table({listOfCity}: Props) {
         setEndDate(date)
     }
 
-    const handleStartTime = (e: any) =>{
+    const handleStartTime = (e: any) => {
         const hh = e.$H;
         const mm = e.$m;
         const ss = e.$s;
@@ -70,7 +72,7 @@ export default function Table({listOfCity}: Props) {
         setStartTime(time)
     }
 
-    const handleEndTime = (e: any) =>{
+    const handleEndTime = (e: any) => {
         const hh = e.$H;
         const mm = e.$m;
         const ss = e.$s;
@@ -78,8 +80,36 @@ export default function Table({listOfCity}: Props) {
         setEndTime(time)
     }
 
-    return (
-        <div>
+    const colorCode = (code: number) => {
+        let style;
+        if (code >= 0 && code <= 50) {
+            return style = {
+                color: "#1D8348",
+            }
+        } else if (code >= 51 && code <= 100) {
+            return style = {
+                color: "#B7950B"
+            }
+        } else if (code >= 101 && code <= 200) {
+            return style = {
+                color: "#BA4A00"
+            }
+        } else if (code >= 201 && code <= 300) {
+            return style = {
+                color: "#922B21"
+            }
+        } else if (code >= 301 && code <= 400) {
+            return style = {
+                color: "#5B2C6F"
+            }
+        } else if (code >= 401 && code <= 500) {
+            return style = {
+                color: "#5B2C6F"
+            }
+        }
+    }
+
+    return (<div>
             <h3 className={"text-primary text-center mb-4 fw-light"}><u>Result of you keyword</u></h3>
             <div className={"d-flex flex-column gap-4 mb-3"}>
                 <select className="form-select mb-3" aria-label="Default select example" onChange={(e) => {
@@ -100,7 +130,7 @@ export default function Table({listOfCity}: Props) {
                                 onChange={(e) => {
                                     handleStartDate(e)
                                 }}
-                                defaultValue={dayjs("2023-04-09")}
+                                defaultValue={dayjs(new Date())}
                             />
                             <DatePicker
                                 label="End Date"
@@ -108,7 +138,7 @@ export default function Table({listOfCity}: Props) {
                                 onChange={(e) => {
                                     handleEndDate(e)
                                 }}
-                                defaultValue={dayjs("2023-04-09")}
+                                defaultValue={dayjs(new Date())}
                             />
                         </div>
                         <div>
@@ -127,14 +157,14 @@ export default function Table({listOfCity}: Props) {
                                 onChange={(e) => {
                                     handleEndTime(e)
                                 }}
-                                defaultValue={dayjs("2022-04-17T09:00:00")}
+                                defaultValue={dayjs("2022-04-17T23:00:00")}
                             />
                         </div>
                     </div>
                 </LocalizationProvider>
             </div>
-            <table className="table table-striped table-bordered text-center">
-                <thead>
+            <table className="table table-striped text-center">
+                <thead className={"thead-dark"}>
                 <tr>
                     <th scope="col">ID</th>
                     <th scope="col">STATION ID</th>
@@ -147,33 +177,50 @@ export default function Table({listOfCity}: Props) {
                     <th scope="col">COLOR CODE</th>
                     <th scope="col">OPTIONS</th>
                 </tr>
+                </thead>
+                <tbody>
                 {result.map((data: any, index: any) => {
-                    for (let i = 0; i < listOfCity.length; i++) {
-                        if (data.areaTH.includes(listOfCity[i]) || data.nameTH.includes(listOfCity[i])) {
-                            return (
-                                //check if data contain the keyword in listOfCity
-                                <tr key={index}>
+                    if (listOfCity.length === 0) {
+                        return (<tr key={index}>
+                                <td>{index + 1}</td>
+                                <td style={colorCode(data.value)}>{data.station_id}</td>
+                                <td style={colorCode(data.value)}>{data.nameTH}</td>
+                                <td style={colorCode(data.value)}>{data.areaTH}</td>
+                                <td style={colorCode(data.value)}>{data.date}</td>
+                                <td style={colorCode(data.value)}>{data.time}</td>
+                                <td style={colorCode(data.value)}>{data.aqi}</td>
+                                <td style={colorCode(data.value)}>{data.value}</td>
+                                <td style={colorCode(data.value)}>{data.color_id}</td>
+                                <td>
+                                    <IconPhoto style={{
+                                        cursor: "pointer"
+                                    }}/>
+                                </td>
+                            </tr>)
+                    } else {
+                        for (let i = 0; i < listOfCity.length; i++) {
+                            if (data.areaTH.includes(listOfCity[i]) || data.nameTH.includes(listOfCity[i])) {
+                                return (<tr key={index}>
                                     <td>{index + 1}</td>
-                                    <td>{data.station_id}</td>
-                                    <td>{data.nameTH}</td>
-                                    <td>{data.areaTH}</td>
-                                    <td>{data.date}</td>
-                                    <td>{data.time}</td>
-                                    <td>{data.aqi}</td>
-                                    <td>{data.value}</td>
-                                    <td>{data.color_id}</td>
+                                    <td style={colorCode(data.value)}>{data.station_id}</td>
+                                    <td style={colorCode(data.value)}>{data.nameTH}</td>
+                                    <td style={colorCode(data.value)}>{data.areaTH}</td>
+                                    <td style={colorCode(data.value)}>{data.date}</td>
+                                    <td style={colorCode(data.value)}>{data.time}</td>
+                                    <td style={colorCode(data.value)}>{data.aqi}</td>
+                                    <td style={colorCode(data.value)}>{data.value}</td>
+                                    <td style={colorCode(data.value)}>{data.color_id}</td>
                                     <td>
                                         <IconPhoto style={{
                                             cursor: "pointer"
                                         }}/>
                                     </td>
-                                </tr>
-                            )
+                                </tr>)
+                            }
                         }
                     }
                 })}
-                </thead>
+                </tbody>
             </table>
-        </div>
-    )
+        </div>)
 }
